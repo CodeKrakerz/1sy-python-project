1984b2ed7033e5b5c5c863e2a0f5d65a6c3b3cba
CodeKrakerz <87552815+CodeKrakerz@users.noreply.github.com>
2022-03-30 20:55:06 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+print("Usman Emmanuel")
+print("i love using pycharm ")
+print('O----')
+print(' ||||')
+print('@' * 10)
+
+
+price = 10
+print(price)
+
+rating = 4.9
+print(rating)
+
+name = 'emmanuel'
+print(name)
+
+is_published = True
+print( is_published)
+
+
+
+patient_name = 'john_smith'
+print(patient_name)
+age = '20 years'
+print(age)
+is_new = True
+print(is_new)
+
+
